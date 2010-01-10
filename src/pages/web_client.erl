8859488e442b027e0b9fed52dbbae921af1ed5d3
troystribling <troy.stribling@gmail.com>
2010-01-10 16:03:06 -0500

    changed mind about client layout. changed back to single instance interface.

--- src/pages/web_client.erl
+++ src/pages/web_client.erl
@@ -27,65 +27,12 @@ connect() ->
     Jid = client_user_model:bare_jid(User),
     #passwd{password=Password} = passwd_model:find(Jid),
     Args = string:join(["/http-bind/", Jid++"/gnos.us", Password], "','"),
-    #panel{body="", actions=#script{script="connect('"++Args++"');"}}.
+    #panel{body="", actions=#script{script="connect('"++Args++"');new GnosusUi();"}}.
 
-%%--------------------------------------------------------------------------------
-client() ->
-    wf:session(clients, []),
-    add_client().
-	
 %%================================================================================
 event(logout) ->
     gnosus_utils:client_user_logout();
 
 %%--------------------------------------------------------------------------------
-event(add_client) ->
-    add_client();
-
-%%--------------------------------------------------------------------------------
-event({remove_client, Client}) ->
-    remove_client(Client);
-
-%%--------------------------------------------------------------------------------
 event(_) -> ok.
-
-%%================================================================================
-add_client() ->
-    ClientId = client_id(),
-    RowId = row_id(),
-    Client = [#panel{body="", id="client-display-toolbar-"++integer_to_list(ClientId)},
-              #panel{body="", id="client-display-"++integer_to_list(ClientId)},
-              #panel{body="", id="client-toolbar-"++integer_to_list(ClientId)}],
-    Body = case row_exists(RowId) of
-               true -> Client;            
-               false -> #panel{body=Client, id="client-row-"++integer_to_list(RowId)}}    
-           end,
-    wf_session(clients, [{client_id, ClientId}, {row_id, RowId}]),       
-    #panel{body=Body, actions=#script{script="GnosusUiMgr.add_client('"++ClientId++"');"}}.
-        
-%%--------------------------------------------------------------------------------
-remove_client(Client) -> ok.
-    
-%%--------------------------------------------------------------------------------
-client_id() ->    
-    {S1,S2,S3} = now(),
-    random:seed(S1,S2,S3),
-    client_id(random:uniform(1000)).   
-    
-client_id(Id) ->
-    case lists:any(fun([{client_id, ClientId}, {row_id, _}]) ->
-                       ClientId =:= Id
-                   end, wf:session(clients)) of
-        true -> client_id();
-        false -> Id
-    end.
-    
-%%--------------------------------------------------------------------------------
-row_id() ->
-    trunc(length(wf:session(clients))/?COLUMNS) + 1.
-    
-%%--------------------------------------------------------------------------------
-row_exits(Id) ->
-    lists:any(fun([{client_id, _}, {row_id, RowId}]) ->
-                  RowId =:= Id
-              end, wf:session(clients)).    
+ 
--- wwwroot/javascript/gnosus_ui.js
+++ wwwroot/javascript/gnosus_ui.js
@@ -1,30 +1,15 @@
 /**********************************************************************************
-ui manager
-**********************************************************************************/
-GnosusUiMgr = {
-    
-    /*----------------------------------------------------------------------------*/    
-    clients: {},
-
-    /*----------------------------------------------------------------------------*/ 
-    add_client: function(client_id) { 
-        GnosusUiMgr.clients[client_id] = new GnosusUi(client_id);       
-    }   
-    remove_client: function(client_id) {
-        GnosusUiMgr.clients[client_id].unbind();
-        delete(GnosusUiMgr.clients[client_id])      
-    }   
-}
-
-/**********************************************************************************
 ui
 **********************************************************************************/
-function GnosusUi(client_id) {
+function GnosusUi() {
     this.handlers = {},
-    this.client_id = client_id,
-    this.client_display = '#client-display-'+this.client_id;
-    this.client_display_toolbar = '#client-display-toolbar-'+this.client_id;
-    this.client_toolbar = '#client-toolbar-'+this.client_id;
+    this.client = '#client';
+    this.client_items_display = '#client-items-display';
+    this.client_messages_display = '#client-messages-display';
+    this.client_items_toolbar = '#client-items-toolbar';
+    this.client_messages_toolbar = '#client-messages-toolbar';
+    $('#client').splitter({type: "v", outline: true, minLeft: 200, sizeLeft: 200, minRight: 500, cookie: "vsplitter"});
+    this.show_roster();
 }
 
 /*--------------------------------------------------------------------------------*/    
@@ -33,7 +18,7 @@ GnosusUi.prototype = {
     /*-------------------------------------------------------------------------------*/    
     unbind: function() {
         for (var evt in this.handlers) {
-            $(this.client_display).unbind(evt, this.handlers[evt]);
+            $(document).unbind(evt, this.handlers[evt]);
         } 
         this.handlers = {};                   
     },
@@ -41,17 +26,16 @@ GnosusUi.prototype = {
     /*-------------------------------------------------------------------------------*/    
     show_roster: function () {
         this.handlers['roster_changed'] = function (ev, roster) {
-            $(this.client_display).empty();
+            $(this.client_items_display).empty();
             var html = ["<ul>"];
             $.each(Gnosus.find_all_contacts(), function (jid) {
-                html.push("<li><div class='item'" + this.show() + "'>");
+                html.push('<li><div class="contact ' + this.show() + '">');
                 html.push(this.name || jid);
                 html.push("</div></li>");
             });
             html.push("</ul>");
-            $(this.client_display).append(html.join(''));
+            $(this.client_items_display).append(html.join(''));
         }
-        $(document).bind('roster_changed', this.handlers['roster_changed']);
-    }
-        
+        $(document).bind('roster_changed', this.handlers['roster_changed'].bind(this));
+    }        
 }
