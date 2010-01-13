eae1aa03d0f7669fba8643e4e69ef714a5d5b0b8
troystribling <troy.stribling@gmail.com>
2010-01-13 00:04:01 -0500

    item select control works.

--- wwwroot/javascript/gnosus_ui.js
+++ wwwroot/javascript/gnosus_ui.js
@@ -5,10 +5,12 @@ function GnosusUi() {
     this.handlers = {},
     this.client = '#client';
     this.client_items_display = '#client-items-display';
-    this.client_messages_display = '#client-messages-display';
     this.client_items_toolbar = '#client-items-toolbar';
+    this.client_item_selected = '#client-item-selected';
+    this.client_item_choices = '#client-item-choices';
+    this.client_messages_display = '#client-messages-display';
     this.client_messages_toolbar = '#client-messages-toolbar';
-    $('#client').splitter({type: "v", outline: true, minLeft: 200, sizeLeft: 200, minRight: 500, cookie: "vsplitter"});
+    $('#client').splitter({type: "v", outline: true, minLeft: 250, sizeLeft: 250, minRight: 500, cookie: "vsplitter"});
     this.show_roster();
 }
 
@@ -44,31 +46,35 @@ GnosusUi.prototype = {
     show_items_toolbar: function(selected, add_item) {
         $(this.client_items_toolbar).empty();
         var item_select = '<div id="client-item-selector">'+ 
-                              '<div id="client-item-selected"><a>' + selected + '</div>' +
+                              '<div id="client-item-selected">' + selected + '</div>' +
                               '<ul id="client-item-choices" style="display: none">' +
                                   '<li>roster</li>' +
                                   '<li>resources</li>' +
                                   '<li>subscriptions</li>' +
-                                  '<li>publication</li>' +
+                                  '<li>publications</li>' +
                               '</ul>' +
                           '</div>';  
         $(this.client_items_toolbar).append(item_select);
-        $('div#client-item-selected').click(function() {
-            $('ul#client-item-choices').toggle();
+        var item_choices = this.client_item_choices;
+        $(this.client_item_selected).click(function() {
+            $(this).toggleClass('open')
+            $(item_choices).toggle();
         });
-    }        
-}
-
-/**********************************************************************************
-item choice selector
-**********************************************************************************/
-function choose_item() {
-    $('#client-item-choices li').hover(
-        function() { 
-            $(this).addClass('choice'); 
-        }, 
-        function() { 
-            $(this).removeClass('choice'); 
-        }
-    ); 
+        this.select_item();
+    }, 
+    
+    /*-------------------------------------------------------------------------------*/    
+    select_item: function() {
+        $(this.client_item_choices+' li').hover(
+            function() {$(this).addClass('choice');}, 
+            function() {$(this).removeClass('choice');}
+        ); 
+        var item_selected = this.client_item_selected;
+        var item_choices = this.client_item_choices;
+        $(this.client_item_choices+' li').click(function() {
+            $(item_selected).toggleClass('open')
+            $(item_selected).text($(this).text());
+            $(item_choices).toggle();
+        }); 
+    }           
 }
